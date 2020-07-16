7b31bdc04a21c4a477859b1d6bc6797d6d21460c
sohabhh@gmail.com <sohabhh@gmail.com>
2020-07-16 11:07:39 +0400

    16-07-2020

---
+++ react-basic/precache-manifest.a51f324f706b9c5376596ea92c3aca8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373a4cfe08a9adf0afc8035532160113",
+    "url": "/www.sohab-hh.com/react-basic/index.html"
+  },
+  {
+    "revision": "2adb4c22005fac10f6f8",
+    "url": "/www.sohab-hh.com/react-basic/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "41555c5ddba5b66f5f5b",
+    "url": "/www.sohab-hh.com/react-basic/static/js/2.6baf1f20.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/www.sohab-hh.com/react-basic/static/js/2.6baf1f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2adb4c22005fac10f6f8",
+    "url": "/www.sohab-hh.com/react-basic/static/js/main.5e898fca.chunk.js"
+  },
+  {
+    "revision": "caf0a37fae2d57bbc36d",
+    "url": "/www.sohab-hh.com/react-basic/static/js/runtime-main.652adf22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/www.sohab-hh.com/react-basic/static/media/logo.5d5d9eef.svg"
+  }
+]);
